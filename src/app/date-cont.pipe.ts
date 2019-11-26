888613c3dc8761ad17fedfdd7aed2d108b6bd693
FrankKamala <frankkamala@gmail.com>
2019-11-26 13:03:12 +0300

    added completion date

--- src/app/date-cont.pipe.ts
+++ src/app/date-cont.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateContPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any): number {
+    let today:Date = new Date (); //get the date and time
+    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
+    var dateDifference = Math.abs(value - todayWithNoTime)//MILISECONDS
+    const secondsInDay = 86400;//secs* mins*24hours
+    var dateDifferenceSeconds = dateDifference * 0.001; //miiliseconds to seconds
+    var dateCont = dateDifferenceSeconds / secondsInDay ;
+
+    if (dateCont >=1 && value >todayWithNoTime){
+      return dateCont;
+    }else{
+      return 0;
+    }
+
   }
 
 }
