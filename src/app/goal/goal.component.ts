73feb67571adf50e450d9066ee33957b34d587a8
FrankKamala <frankkamala@gmail.com>
2019-11-26 13:03:30 +0300

    added new dates

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -9,11 +9,11 @@ export class GoalComponent implements OnInit {
 
     goals:Goal[] = [
       //new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
-      new Goal(1,'watch Finding Nemo', 'Check on Netflix', new Date( 2019, 6 ,26)),
-      new Goal(2, 'Buycookies' , 'Buy from Morio', new Date(2019, 6, 27)),
+      new Goal(1,'watch Finding Nemo', 'Check on Netflix', new Date( 2019, 12 ,30)),
+      new Goal(2, 'Buycookies' , 'Buy from Morio', new Date(2019, 12, 30)),
 
-      new Goal(3,'Get new Phone Case','Buy from CBD', new Date(2020,1,1)),
-      new Goal(4,'Get Dog Food', 'Purchase from DogHouse', new Date(2019,12,26)),
+      new Goal(3,'Get new Phone Case','Buy from CBD', new Date(2020,12,30)),
+      new Goal(4,'Get Dog Food', 'Purchase from DogHouse', new Date(2019,12,29)),
       new Goal(5,'Solve math homework','INAUMA BUT...', new Date(2019,11,29)),
 
     ];
